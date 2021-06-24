ddb10999eac627fd0021de40ce30bf71a91b8d96
Евгений <evxeen@gmail.com>
2021-06-24 20:39:53 +0300

    add.resetting the game

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const dice = document.querySelector('.dice');
 
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold'); 
+const btnNew = document.querySelector('.btn--new');
 
-const dice = document.querySelector('.dice');
-dice.classList.add('hidden')
+let scores, scoreResult, activePlayer, playing;  
+ 
+const init = function() {
+  scores = [0, 0];
+  scoreResult = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let scoreResult = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  dice.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
+
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -42,13 +61,16 @@ btnHold.addEventListener('click', function() {
     scores[activePlayer] += scoreResult;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
   
-    if (scores[activePlayer] >= 10) { 
+    if (scores[activePlayer] >= 100) { 
       dice.classList.add('hidden'); 
       playing = false;
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
       document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
     } else {
-      switchPlayer()
+      switchPlayer();
     }
   }
 })
+
+btnNew.addEventListener('click', init);
+
